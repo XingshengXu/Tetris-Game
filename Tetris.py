f97ad0131ec2509a4119f6a9bc4f4ab1d2d40078
Xingsheng Xu <xuxingsheng163@gmail.com>
2023-04-27 17:01:36 -0700

    Version 0.3

--- Tetris.py
+++ Tetris.py
@@ -6,64 +6,82 @@ from settings import *
 pg.init()
 
 
-class Tetrimino(pg.sprite.Sprite):
-    def __init__(self, tetriminosType):
+def load_sprite_sheet(sheet_path, sprite_width, sprite_height, needScale2x=False, customScale=False, size=None):
+    """Splits a sprite sheet into multiple sub-sprites based on the given width and height, 
+    and optionally scales them."""
+    image = pg.image.load(sheet_path).convert()
+    image_num = image.get_width() // sprite_width
+
+    sprites = []
+    for i in range(image_num):
+        surface = pg.Surface((sprite_width, sprite_height))
+        rect = pg.Rect(i * sprite_width, 0, sprite_width, sprite_height)
+        surface.blit(image, (0, 0), rect)
+        if needScale2x:
+            surface = pg.transform.scale2x(surface)
+        elif customScale:
+            surface = pg.transform.scale(surface, size)
+        sprites.append(surface)
+    return sprites
+
+
+class Block(pg.sprite.Sprite):
+    def __init__(self, tetromino, coord):
         super().__init__()
-        self.type = tetriminosType
-        self.frame_counter = 0
-        self.is_horizontal = True
+        self.tetromino = tetromino
+        self.coord = vec(coord) + TETROMINO_SPAWN_OFFSET
+        self.image = self.tetromino.image
+        self.rect = self.image.get_rect(topleft=self.coord * GRID_WIDTH)
+        self.dir = 'down'
 
-        # Load Tetrimino Image
-        self.image = pg.transform.scale(pg.image.load(
-            TETRIMINOS[self.type]).convert(), TETRIMINO_DIMENSIONS[self.type])
-        self.rect = self.image.get_rect(topleft=TETRIMINO_SPAWN_POS)
+    def tetromino_fall(self):
+        new_coord = self.coord + MOVEMENTS['down']
+        if not self.check_collision(new_coord):
+            self.coord = new_coord
+            self.rect.topleft = self.coord * GRID_WIDTH
 
-    def tetrimino_fall(self):
-        if self.frame_counter % 15 == 0:
-            self.rect.y += MOVING_SPEED
+    def tetromino_move(self):
+        key = pg.key.get_pressed()
+        if key[pg.K_LEFT]:
+            self.dir = 'left'
+        elif key[pg.K_RIGHT]:
+            self.dir = 'right'
 
-    def tetrimino_rotate(self):
-        if self.type == 'I':
-            if self.is_horizontal:
-                self.rect.x += GRID_WIDTH
-                self.rect.y -= GRID_WIDTH
-                self.image = pg.transform.rotate(self.image, 90)
-                self.is_horizontal = False
-            else:
-                # self.rect.x -= GRID_WIDTH
-                # self.rect.y += GRID_WIDTH
-                self.image = pg.transform.rotate(self.image, -90)
-                self.is_horizontal = True
-        elif self.type == 'S':
-            self.rect.x += GRID_WIDTH
-            self.rect.y -= GRID_WIDTH
-        elif self.type == 'Z':
-            self.rect.x += GRID_WIDTH
-            self.rect.y -= GRID_WIDTH
-        # self.image = pg.transform.rotate(self.image, 90)
+        new_coord = self.coord + MOVEMENTS[self.dir]
+        if not tetrominos.sprites.check_collision(new_coord):
+            self.coord = new_coord
+            self.rect.topleft = self.coord * GRID_WIDTH
 
-    def tetrimino_move(self):
-        if self.frame_counter % 3 == 0:
-            key = pg.key.get_pressed()
-            if key[pg.K_DOWN]:
-                self.rect.y += MOVING_SPEED
-            elif key[pg.K_LEFT] and self.rect.left - MOVING_SPEED >= 0:
-                self.rect.x -= MOVING_SPEED
-            elif key[pg.K_RIGHT] and self.rect.right + MOVING_SPEED <= WIDTH - HUD_WIDTH:
-                self.rect.x += MOVING_SPEED
-            elif key[pg.K_UP]:
-                self.tetrimino_rotate()
-
-    def tetrimino_locked(self):
-        if self.rect.bottom + MOVING_SPEED > HEIGHT:
-            pg.event.post(pg.event.Event(TETRIMINO_LOCKED))
-            self.kill()
+    def check_collision(self, new_coord):
+        x, y = int(new_coord.x), int(new_coord.y)
+        if 0 <= x < FIELD_WIDTH and y < FIELD_HEIGHT:
+            return False
+        return True
 
     def update(self):
-        self.frame_counter += FRAME_INCREMENT
-        self.tetrimino_locked()
-        self.tetrimino_fall()
-        self.tetrimino_move()
+        if game.anim_trigger:
+            self.tetromino_fall()
+        if game.control_trigger:
+            self.tetromino_move()
+
+
+class Tetromino():
+    def __init__(self, tetrominosType):
+        self.type = tetrominosType
+
+        # Load Block Image
+        block_img = load_sprite_sheet(
+            TETROMINO_TILES, TILE_WIDTH, TILE_HEIGHT,
+            customScale=True, size=(GRID_WIDTH, GRID_WIDTH))
+        self.image = block_img[TETROMINO_TILETYPE[self.type]]
+        self.blocks = [Block(self, coord) for coord in TETROMINOES[self.type]]
+
+    def is_collide(self, block_pos):
+        return any(map(Block.is_collide, self.blocks, block_pos))
+#     def Tetromino_locked(self):
+#         if self.rect.bottom + MOVING_SPEED > HEIGHT:
+#             pg.event.post(pg.event.Event(Tetromino_LOCKED))
+#             self.kill()
 
 
 class Game:
@@ -75,8 +93,9 @@ class Game:
     def setup(self):
         self.game_active = False
         self.clock = pg.time.Clock()
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pg.display.set_mode(SCREEN_SIZE)
         pg.display.set_caption('Tetris')
+        self.set_timer()
 
     def load_resources(self):
         self.load_images()
@@ -86,23 +105,35 @@ class Game:
 
     def draw_grid(self):
         x, y = 0, 0
-        for _ in range(ROW_NUMBER):
+        for _ in range(ROW_NUMBER + 1):
+            pg.draw.line(self.screen, 'white', (0, y), (WIDTH-HUD_WIDTH, y), 1)
             y += GRID_WIDTH
-            pg.draw.line(self.screen, 'white', (0, y), (WIDTH-HUD_WIDTH, y))
-        for _ in range(COLUMN_NUMBER):
+        for _ in range(COLUMN_NUMBER + 1):
+            pg.draw.line(self.screen, 'white', (x, 0), (x, HEIGHT), 1)
             x += GRID_WIDTH
-            pg.draw.line(self.screen, 'white', (x, 0), (x, HEIGHT))
 
     def draw_background(self):
         self.screen.fill('black')
 
+    def set_timer(self):
+        self.anim_trigger = False
+        self.control_trigger = False
+        pg.time.set_timer(FALL_TRIGGER, FALL_FREQ)
+        pg.time.set_timer(CONTROL_TRIGGER, CONTROL_FREQ)
+
     def handle_events(self):
+        self.anim_trigger = False
+        self.control_trigger = False
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 exit()
-            if event.type == TETRIMINO_LOCKED:
-                tetriminos.add(Tetrimino('I'))
+            if event.type == FALL_TRIGGER:
+                self.anim_trigger = True
+            if event.type == CONTROL_TRIGGER:
+                self.control_trigger = True
+            # if event.type == Tetromino_LOCKED:
+            #     Tetrominos.add(Tetromino('I'))
 
     def main_loop(self):
         """This is the game main loop."""
@@ -116,19 +147,21 @@ class Game:
             self.draw_background()
 
             # Draw Sprites
-            tetriminos.draw(self.screen)
+            tetrominos.draw(self.screen)
             # Update Sprites
-            tetriminos.update()
+            tetrominos.update()
+
             # Draw Grid
             self.draw_grid()
+
             pg.display.update()
 
 
 # Create Class Instances and Add Sprites
 game = Game()
 
-tetriminos = pg.sprite.Group()
-tetriminos.add(Tetrimino('I'))  # choice(list(TETRIMINOS.keys()))
+tetrominos = pg.sprite.SingleGroup()
+tetrominos.add(Tetromino('Z').blocks)
 
 # Run Main Loop
 game.main_loop()
--- settings.py
+++ settings.py
@@ -6,85 +6,67 @@ The settings module for the "Tetris" game. This module contains all the default 
 These settings can be easily accessed and used throughout the game by importing the module into the main game file.
 """
 
+# Define a reference to the Vector2 class
+vec = pg.math.Vector2
+
 # Game Play Settings
-FPS = 30
-MOVING_SPEED = 30
-FRAME_INCREMENT = 1
+FPS = 60
+MOVING_SPEED = 40
+
+MOVEMENTS = {
+    'left': vec(-1, 0),
+    'right': vec(1, 0),
+    'down': vec(0, 1)
+}
 
 # Game Dimensions
-WIDTH, HEIGHT = 440, 600  # Game Window Display Dimensions
-HUD_WIDTH, HUD_HEIGHT = 140, 600  # HUD Dimensions
-GRID_WIDTH = 30  # Grid Width
+GRID_WIDTH = 40  # Grid Width
+# HUD Dimensions
+HUD_SIZE = HUD_WIDTH, HUD_HEIGHT = GRID_WIDTH * 5, GRID_WIDTH * 20
+# Window Display Dimensions
+SCREEN_SIZE = WIDTH, HEIGHT = GRID_WIDTH * 15, GRID_WIDTH * 20
+# Game Coordinate Field Dimensions
+FIELD_SIZE = FIELD_WIDTH, FIELD_HEIGHT = 10, 20
 ROW_NUMBER = HEIGHT // GRID_WIDTH  # Row Number
 COLUMN_NUMBER = (WIDTH - HUD_WIDTH) // GRID_WIDTH  # Column Number
-TETRIMINO_SPAWN_POS = (120, 0)
-
-TETRIMINO_DIMENSIONS = {
-    'I': (120, 30),
-    'J': (90, 60),
-    'L': (90, 60),
-    'O': (60, 60),
-    'S': (90, 60),
-    'T': (90, 60),
-    'Z': (90, 60)
-}
+TILE_SIZE = TILE_WIDTH, TILE_HEIGHT = 8, 8  # Tetromino Tile Dimensions
+# Tetromino Spawn Position Offset
+TETROMINO_SPAWN_OFFSET = vec(FIELD_WIDTH // 2, 0)
 
 # Font Path
 
 # Game Events
-TETRIMINO_LOCKED = pg.USEREVENT + 1
+FALL_TRIGGER = pg.USEREVENT + 1
+CONTROL_TRIGGER = pg.USEREVENT + 2
 
-# Spawn Frequence and Delay Time
+# TETROMINO_LOCKED = pg.USEREVENT + 1
 
+# Event Frequence and Delay Time
+FALL_FREQ = 500  # Tetrimino Fall Frequency
+CONTROL_FREQ = 100  # Tetrimino Control Frequency
 # Image Path
-TETRIMINOS = {
-    'I': 'assets/tetriminos/I tetrimino.png',
-    'J': 'assets/tetriminos/J tetrimino.png',
-    'L': 'assets/tetriminos/L tetrimino.png',
-    'O': 'assets/tetriminos/O tetrimino.png',
-    'S': 'assets/tetriminos/S tetrimino.png',
-    'T': 'assets/tetriminos/T tetrimino.png',
-    'Z': 'assets/tetriminos/Z tetrimino.png'
+TETROMINO_TILES = 'assets/tetrominos/tetromino_tiles.png'
+
+TETROMINO_TILETYPE = {
+    'O': 0,
+    'T': 1,
+    'S': 2,
+    'Z': 3,
+    'J': 4,
+    'L': 5,
+    'I': 6
 }
 
 # Sound Path
 
 
-# The relative positions for each rotation of the Tetrimino types.
-# The values represent the relative (x, y) coordinates for each block
-# in the Tetrimino, with respect to the center block.
-TETRIMINO_ROTATIONS = {
-    'I': [
-        [(-1, 0), (0, 0), (1, 0), (2, 0)],
-        [(1, -1), (1, 0), (1, 1), (1, 2)]
-    ],
-    'O': [
-        [(0, 0), (0, 1), (1, 0), (1, 1)]
-    ],
-    'J': [
-        [(-1, 1), (-1, 0), (0, 0), (1, 0)],
-        [(0, -1), (1, -1), (0, 0), (0, 1)],
-        [(1, -1), (1, 0), (0, 0), (-1, 0)],
-        [(0, 1), (-1, 1), (0, 0), (0, -1)]
-    ],
-    'L': [
-        [(-1, 0), (0, 0), (1, 0), (1, 1)],
-        [(0, -1), (0, 0), (0, 1), (1, 1)],
-        [(1, 0), (0, 0), (-1, 0), (-1, -1)],
-        [(0, 1), (0, 0), (0, -1), (-1, -1)]
-    ],
-    'S': [
-        [(0, 0), (1, 0), (-1, 1), (0, 1)],
-        [(0, 0), (0, 1), (1, -1), (1, 0)]
-    ],
-    'T': [
-        [(-1, 0), (0, 0), (1, 0), (0, 1)],
-        [(0, -1), (0, 0), (0, 1), (1, 0)],
-        [(1, 0), (0, 0), (-1, 0), (0, -1)],
-        [(0, 1), (0, 0), (0, -1), (-1, 0)]
-    ],
-    'Z': [
-        [(-1, 0), (0, 0), (0, 1), (1, 1)],
-        [(0, 0), (0, 1), (1, 0), (1, -1)]
-    ]
+# Tetromino Shapes
+TETROMINOES = {
+    'O': [(0, 0), (-1, 0), (-1, -1), (0, -1)],
+    'T': [(0, 0), (-1, 0), (1, 0), (0, -1)],
+    'S': [(0, 0), (-1, 0), (0, -1), (1, -1)],
+    'Z': [(0, 0), (1, 0), (-1, -1), (0, -1)],
+    'J': [(0, 0), (-1, 0), (1, 0), (-1, -1)],
+    'L': [(0, 0), (-2, 0), (-1, 0), (0, -1)],
+    'I': [(0, 0), (-2, 0), (-1, 0), (1, 0)]
 }
